674425ed2ef51fb47b75fc9b4123a7289d41200b
depgap <117119820+depgap@users.noreply.github.com>
2024-05-29 12:00:31 +0000

    added inventory limits

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 public class Player {
     private double health;
     private ArrayList<String> inventory;
+    private final int MAX_ITEMS = 5;
 
     public Player() {
         health = 100;
@@ -22,15 +23,21 @@ public class Player {
         System.out.println();
     }
     public void addItem(String item) {
-        inventory.add(item);
-        sortInventory();
-        System.out.println("Added " + item + " to inventory");
+        if (inventory.size() < MAX_ITEMS) {
+            inventory.add(item);
+            sortInventory();
+            System.out.println("Added " + item + " to inventory");
+        }
+        else {
+            System.out.println("Your inventory is full");
+        }
     }
     public void removeItem(String item) {
         for (int i = 0; i < inventory.size(); i++) {
             if (item.equals(inventory.get(i))) {
                 System.out.println("Removed " + inventory.get(i));
                 inventory.remove(i);
+                sortInventory();
                 return;
             }
         }
